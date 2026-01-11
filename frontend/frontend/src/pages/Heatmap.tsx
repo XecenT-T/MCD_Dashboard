8f9d035980a670ac68a24d686a40c9187383d869
Shagun Agarwal <agarwalshagun64@gmail.com>
2026-01-11 14:55:43 +0530

    fix

--- frontend/frontend/src/pages/Heatmap.tsx
+++ frontend/frontend/src/pages/Heatmap.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useAuth } from '../context/AuthContext';
 import api from '../api/axios';
 import DashboardLayout from '../components/DashboardLayout';
-import LiveLocationMap, { GenericAttendanceRecord } from '../components/LiveLocationMap';
+import LiveLocationMap, { type GenericAttendanceRecord } from '../components/LiveLocationMap';
 
 const Heatmap = () => {
     const { token } = useAuth();
